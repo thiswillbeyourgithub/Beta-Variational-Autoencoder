db386e7ac36e0024032774b48cabcdb180dff926
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-12-05 14:39:26 +0100

    docs

--- bvae.py
+++ bvae.py
@@ -226,7 +226,12 @@ class OptimizedBVAE(BaseEstimator):
         self.params = kwargs
 
     def fit(self, dataset):
-        """Fit the model according to the given training data."""
+        """
+        Fit multiple model on the given data, with different hidden_dim values
+        and beta values.
+
+        Returns a fitted model.
+        """
         hidden_dim_candidates = [int(len(dataset) * ratio) for ratio in [0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 0.9]]
         beta_candidates = [5]
         batch_size = dataset.shape[0]
